43576a270ced3b67b0c183f8c7e494af081c568d
Janvdk5 <s2543606@ed.ac.uk>
2026-04-20 17:18:52 +0200

    add: gtrack module

--- streaming_base/processing/processing.py
+++ streaming_base/processing/processing.py
@@ -1,5 +1,9 @@
 import numpy as np
 from scipy.signal import convolve2d
+from scipy.ndimage import median_filter
+
+from sklearn.cluster import DBSCAN
+from gtrack.config import Detection
 
 # implement this function to accumulate the time domain data 
 def get_accumulated_time_data(current_range_data, range_fft_s):
@@ -77,6 +81,83 @@ def cfar_ca_2d(power_map,
 
     return detection_map
 
+
+def beamform_2d(beat_freq_data, phi_s, phi_e, phi_res, theta_s, theta_e, theta_res, x_locs, z_locs, r_idxs, radar_params):
+    """
+    Performs 2D beamforming along the azimuth (horizontal) dimension, this results in a bird eye view image.
+    - beat_freq_data: beat data AKA the range FFT (size: num_x_stps * num_z_stps * num TX * num RX, num ADC samples)
+    - phi_s: first azimuth angle that you want to start computing 
+    - phi_e: last azimuth angle that you want to compute 
+    - phi_res: resolution of the azimuth angles you want to compute
+    - theta_s: first elevation angle that you want to start computing 
+    - theta_e: last elevation angle that you want to compute 
+    - theta_res: resolution of the elevation angles you want to compute
+    - x_locs: x coordinate of antenna locations
+    - z_locs: z coordinate of antenna locations
+    - r_idx: range bins to calculate 
+    - radar_parms: radar_params if needed 
+
+    Returns:
+    - sph_pwr: beamformed result (size: n_phi, n_theta, n_range)
+    - phi: array of azimuth angles 
+    - theta: array of elevation angles 
+    """
+    lam = radar_params['lm']
+    numSamples = radar_params['adc_samples']
+
+    phiRange = np.arange(phi_s, phi_e + phi_res, phi_res)
+    thetaRange = np.arange(theta_s, theta_e + theta_res, theta_res)
+
+    numPhi = len(phiRange)
+    numTheta = len(thetaRange)
+
+    # Crop binms
+    rangeBinStart = 100
+    rangeBinEnd = 140
+    numRangeBins = rangeBinEnd - rangeBinStart # clean up to mach r_idxs
+
+    sph_pwr = np.zeros((numPhi, numTheta, numRangeBins))
+
+    print(f"Xlocs shape: {x_locs.shape}, Zlocs shape: {z_locs.shape}, beat_freq_data shape: {beat_freq_data.shape}")
+
+    xVirt = x_locs
+    zVirt = z_locs
+
+    # 1. Mix RX and TX, crop num inputs
+    mixedSignal = beat_freq_data
+    
+    # 3. Compute range fft
+    rangeFFT = np.fft.fft(mixedSignal, axis=-1)[:, r_idxs]
+
+    for i, phi in enumerate(phiRange):
+
+        # 2. Multiply the resulting signal on each antenna with exp(j*phi) and sum signals
+        phiRads = np.deg2rad(phi)
+
+        for j, theta in enumerate(thetaRange):
+            thetaRads = np.deg2rad(theta)
+
+            xPhase = (np.sin(thetaRads) * np.cos(phiRads))
+            zPhase = (np.cos(thetaRads))
+
+            xPart = xVirt * xPhase
+            zPart = zVirt * zPhase
+            phase = xPart + zPart
+            
+            steeringVector = np.exp(1j * 2*np.pi/lam * (phase))
+            steeringVector = steeringVector.reshape(-1,1)
+            
+            beamformedSignal = np.sum(steeringVector * rangeFFT, axis=0)
+            
+            sph_pwr[i, j, :] = np.abs(beamformedSignal)**2 
+            
+
+            # 4. repeat in every direction
+    return sph_pwr, np.deg2rad(phiRange), np.deg2rad(thetaRange)
+
+
+
+
 def process_frame(range_fft, cfar_params):
     """
     Process a single frame of range FFT data to detect targets using CFAR.
@@ -143,3 +224,45 @@ def process_frame_2d(range_fft, cfar_params):
 
     return dets
 
+def compute_dbscan(output_top, r_idxs, phi, eps=0.5, min_samples=5, p_treshold= 98):
+    """
+    Compute DBSCAN clustering on the output of the beamforming process.
+
+    Parameters
+    ----------
+    output_top : np.ndarray
+        The output of the beamforming process, typically a 2D array.
+    r_idxs : np.ndarray
+        The range indices corresponding to the output.
+    phi : np.ndarray
+        The azimuth angles corresponding to the output.
+    eps : float
+        The maximum distance between two samples for one to be considered as in the neighborhood of the other.
+    min_samples : int
+        The number of samples in a neighborhood for a point to be considered as a core point.
+
+    Returns
+    -------
+    db : DBSCAN
+        The fitted DBSCAN model containing the cluster labels.
+    """
+
+    # Build full coordinate grid
+    phi_rad_2d, r_idxs_2d = np.meshgrid(phi, r_idxs, indexing='ij')  # shape: (180, 140)
+
+    x_coords_m = np.cos(phi_rad_2d) * r_idxs_2d  # shape: (180, 140)
+    z_coords_m = np.sin(phi_rad_2d) * r_idxs_2d  # shape: (180, 140)
+
+    # Flatten for DBSCAN
+    points = np.stack([x_coords_m.ravel(), z_coords_m.ravel()], axis=1)
+    powers = output_top.ravel()
+
+    # Keep only high-power points
+    threshold = np.percentile(powers, p_treshold)
+    valid_mask = powers > threshold
+    points_thresh = points[valid_mask]
+
+    # DBSCAN
+    db = DBSCAN(eps = 0.5, min_samples=min_samples).fit(points_thresh)
+
+    return db
