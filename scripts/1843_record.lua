d30c172d2468239afbef399c90da1019e155c6fc
Janvdk5 <s2543606@ed.ac.uk>
2026-04-30 10:20:50 +0200

    update ML files and stop tracking config lua scripts

--- scripts/1843_record.lua
+++
@@ -1,8 +0,0 @@
-capture_file               =   "lowres2"
-
---TODO: edit this path!!
-SAVE_DATA_PATH = "D:\\\\GitHub\\\\COM-304-Rat-Detection\\\\\\\\data\\\\" .. capture_file .. ".bin"
-
-ar1.CaptureCardConfig_StartRecord(SAVE_DATA_PATH, 1)
-ar1.StartFrame()
-
